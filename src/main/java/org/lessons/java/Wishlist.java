c1e056e75c1d869f94e380ed9c26b14bd2c9e4f7
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-23 15:06:55 +0200

    add: array creates through while loop

--- src/main/java/org/lessons/java/Wishlist.java
+++ src/main/java/org/lessons/java/Wishlist.java
@@ -8,12 +8,33 @@ public class Wishlist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Gift> gifts = new ArrayList<>();
+        boolean keepRunning = true;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("What do you want for Christmas?");
-            String gift = scanner.nextLine();
-            gifts.add(new Gift(gift));
-        } System.out.println("This year for Christmas you want:");
-        System.out.println(gifts);
+        // for (int i = 0; i < 3; i++) {
+        //     System.out.println("What do you want for Christmas?");
+        //     String gift = scanner.nextLine();
+        //     gifts.add(new Gift(gift));
+        // } System.out.println("This year for Christmas you want:");
+        // System.out.println(gifts);
+
+        while(keepRunning){
+            System.out.println("Wanna add something to your wishlist?");
+            System.out.print("Answer with (y)es or (n)o: ");
+            String yesOrNo = scanner.nextLine().toLowerCase();
+            if(yesOrNo.equals("y")){
+                System.out.println("What do you want for Christmas?");
+                String gift = scanner.nextLine();
+                gifts.add(new Gift(gift));  
+            } else {
+                keepRunning = false;
+            }
+        }
+
+        if(gifts.isEmpty()){
+            System.out.println("You sure you don't want anything for Christmas?");
+        } else {
+            System.out.println("This year for Christmas you want:");
+            System.out.println(gifts);
+        }
     }
 }
