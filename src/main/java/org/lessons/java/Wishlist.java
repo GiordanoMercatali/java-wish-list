d72acee8abcff4db21cfc0f82295fd8aa3b2436e
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-23 16:07:14 +0200

    removed: gift file added: gifts converted to list of strings

--- src/main/java/org/lessons/java/Wishlist.java
+++ src/main/java/org/lessons/java/Wishlist.java
@@ -1,13 +1,14 @@
 package org.lessons.java;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Wishlist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Gift> gifts = new ArrayList<>();
+        List<String> gifts = new ArrayList<>();
         boolean keepRunning = true;
 
         // for (int i = 0; i < 3; i++) {
@@ -24,7 +25,7 @@ public class Wishlist {
             if(yesOrNo.equals("y")){
                 System.out.println("What do you want for Christmas?");
                 String gift = scanner.nextLine();
-                gifts.add(new Gift(gift));  
+                gifts.add(gift);  
             } else {
                 keepRunning = false;
             }
@@ -34,6 +35,7 @@ public class Wishlist {
             System.out.println("You sure you don't want anything for Christmas?");
         } else {
             System.out.println("This year for Christmas you want:");
+            Collections.sort(gifts);
             System.out.println(gifts);
         }
     }
