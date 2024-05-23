d1eccc875f9a9fde15d59340aed700afdc05911c
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-23 14:36:19 +0200

    scaffolding

---
+++ src/main/java/org/lessons/java/Gift.java
@@ -0,0 +1,13 @@
+package org.lessons.java;
+
+public class Gift {
+    private String name;
+
+    public Gift(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/java/org/lessons/java/Wishlist.java
@@ -0,0 +1,10 @@
+package org.lessons.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wishlist {
+    public static void main(String[] args) {
+        List<Gift> gifts = new ArrayList<>();
+    }
+}
