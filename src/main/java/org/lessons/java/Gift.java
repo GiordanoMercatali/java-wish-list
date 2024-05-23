0bfb063c8ff184333143a797f3bf1d6242ab0195
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-23 14:53:25 +0200

    add: test for cycle that prints wishlist

--- src/main/java/org/lessons/java/Gift.java
+++ src/main/java/org/lessons/java/Gift.java
@@ -10,4 +10,9 @@ public class Gift {
     public String getName(){
         return name;
     }
+
+    @Override
+    public String toString() {
+      return name;
+    }
 }
--- src/main/java/org/lessons/java/Wishlist.java
+++ src/main/java/org/lessons/java/Wishlist.java
@@ -2,9 +2,18 @@ package org.lessons.java;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Wishlist {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         List<Gift> gifts = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("What do you want for Christmas?");
+            String gift = scanner.nextLine();
+            gifts.add(new Gift(gift));
+        } System.out.println("This year for Christmas you want:");
+        System.out.println(gifts);
     }
 }
